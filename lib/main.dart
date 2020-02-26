9fac1f6a7afd11a5567af131f92fadb746bd7609
alexnyombi <aleksnyombie@gmail.com>
2020-02-27 02:04:41 +0300

    finale

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(MyApp());
 
@@ -14,34 +14,3 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class RandomWords extends StatefulWidget{
-  @override
-  RandomWordsState createState() => RandomWordsState();
-}
-
-class RandomWordsState extends State<RandomWords>{
-  final _randomPairs = <WordPair>[];
-  Widget _buildList(){
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item){
-        if(item.isOdd) return Divider();
-        final index = item ~/ 2;
-        if(index >= _randomPairs.length){
-          _randomPairs.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_randomPairs[index]);
-      },
-    );
-  }
-  Widget _buildRow(WordPair pair){
-    return ListTile(title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18),),);
-  }
-  Widget build(BuildContext context){
-    
-    return Scaffold(
-        appBar: AppBar(title: Text('Word Pair Generator')),
-        body: Center(child: _buildList())
-    );
-  }
-}
